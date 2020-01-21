b3a7c2a517a72c787723436ff020217629afadeb
spyderpz <116122@student.drenthecollege.nl>
2020-01-21 15:59:47 +0100

    changed css and design

--- Controller/js/agenda.js
+++ Controller/js/agenda.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
 
-    if(confirm("\t\t\t\tAccepteert u cookies ? \n\n" +
+    if(getCookie("cookies") || confirm("\t\t\t\tAccepteert u cookies ? \n\n" +
         " De cookies zullen worden gebruikt om de informatie \n " +
         "van u gemaakte afspraak op te slaan zodat u deze later weer kunt verwijderen")){
+        document.cookie = "cookies=True";
     }else{
         window.location.replace('home.php');
     }
@@ -285,4 +286,20 @@ $(document).ready(function () {
         });
     });
 });
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
--- View/php/contact.php
+++ View/php/contact.php
@@ -1,3 +1,21 @@
 <?php
 include_once("header.php");
 ?>
+
+<html>
+<head>
+</head>
+<body>
+<div class="pageWrapper">
+    <div class="contentWrapper">
+
+        <div class="kapperInfo"><p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p></div>
+        <div class="KapperImg"></div>
+    </div>
+</div>
+</body>
+
+</html>
+<?php
+include_once("footer.php");
+?>
